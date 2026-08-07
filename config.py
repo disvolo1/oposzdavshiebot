87ccdb925f91d0a860ed24bd6bcf56602f522d1c
disvolo1 <denislocb17@gmail.com>
2026-08-07 14:19:19 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+ADMIN_IDS = [
+    int(x) for x in os.getenv("ADMIN_IDS", "").split(",")
+    if x.strip()
+]
+
+DB_PATH = "data/bot.db"
+TIMEZONE = "Europe/Moscow"
