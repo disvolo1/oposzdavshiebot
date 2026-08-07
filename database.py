6da9fe9a3566f495daf34227b7a4adb7c380e2e9
disvolo1 <denislocb17@gmail.com>
2026-08-07 18:32:52 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,41 +1,135 @@
 import os
+
 from dotenv import load_dotenv
-from supabase import create_client, Client
+from supabase import create_client
+
 
 load_dotenv()
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-print("SUPABASE URL:", SUPABASE_URL)
+SUPABASE_URL = os.getenv(
+    "SUPABASE_URL"
+)
 
-if not SUPABASE_URL or not SUPABASE_KEY:
-    raise Exception("Нет SUPABASE_URL или SUPABASE_KEY в переменных окружения")
+SUPABASE_KEY = os.getenv(
+    "SUPABASE_KEY"
+)
 
 
-supabase: Client = create_client(
+supabase = create_client(
     SUPABASE_URL,
     SUPABASE_KEY
 )
 
 
-async def save_late_request(user_id: int, username: str, text: str):
+
+# сохраняем заявку игрока
+
+async def save_late_request(
+    user_id,
+    username,
+    tournament
+):
 
     data = {
         "user_id": user_id,
         "username": username,
-        "text": text
+        "tournament": tournament
     }
 
-    print("SENDING:", data)
 
-    result = (
+    supabase.table(
+        "late_requests"
+    ).insert(
+        data
+    ).execute()
+
+
+
+# получаем всех опоздавших на турнир
+
+async def get_late_users(
+    tournament
+):
+
+    response = (
         supabase
         .table("late_requests")
-        .insert(data)
+        .select("username")
+        .eq(
+            "tournament",
+            tournament
+        )
         .execute()
     )
 
-    print("SUPABASE RESPONSE:", result)
 
-    return result
+    users = []
+
+
+    for row in response.data:
+
+        users.append(
+            row["username"]
+        )
+
+
+    return users
+
+
+
+# получаем id сообщения админа
+
+async def get_admin_message(
+    tournament
+):
+
+    response = (
+        supabase
+        .table("late_requests")
+        .select("admin_message_id")
+        .eq(
+            "tournament",
+            tournament
+        )
+        .not_.is_(
+            "admin_message_id",
+            "null"
+        )
+        .limit(1)
+        .execute()
+    )
+
+
+    if response.data:
+
+        return response.data[0][
+            "admin_message_id"
+        ]
+
+
+    return None
+
+
+
+# сохраняем id сообщения админа
+
+async def save_admin_message(
+    tournament,
+    message_id
+):
+
+    (
+        supabase
+        .table("late_requests")
+        .update(
+            {
+                "admin_message_id": message_id
+            }
+        )
+        .eq(
+            "tournament",
+            tournament
+        )
+        .execute()
+    )
